e83f530b24d509089130fb5d3143671bbba3987e
Petter Holmström <petter.holmstrom@outlook.com>
2018-02-20 17:29:34 +0200

    Trying out MongoDB.

---
+++ nlsmap-mongodb/src/main/java/net/pkhapps/nlsmap/mongodb/importer/maastotiedot/OsoitepisteetImporter.java
@@ -0,0 +1,45 @@
+package net.pkhapps.nlsmap.mongodb.importer.maastotiedot;
+
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Indexes;
+import org.bson.BsonDocument;
+import org.bson.BsonInt64;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Iterator;
+import java.util.Map;
+
+class OsoitepisteetImporter extends AbstractImporter {
+
+    OsoitepisteetImporter(@NotNull Iterator<Map<String, Object>> featureIterator,
+                          @NotNull MongoDatabase mongoDatabase) {
+        super(featureIterator, mongoDatabase.getCollection("osoitepisteet", BsonDocument.class));
+        getMongoCollection().createIndex(Indexes.geo2dsphere("sijainti"));
+        getMongoCollection().createIndex(Indexes.compoundIndex(
+                Indexes.text("nimi_ruotsi"),
+                Indexes.text("nimi_suomi")));
+        getMongoCollection().createIndex(Indexes.hashed("kuntatunnus"));
+    }
+
+    @Override
+    @NotNull BsonDocument featureToDocument(@NotNull Map<String, Object> feature) {
+        BsonDocument document = new BsonDocument();
+        document.append("_id", new BsonInt64((Long) feature.get("gid")));
+
+        appendValueIfNotNull(document, "sijaintitarkkuus", feature);
+        appendValueIfNotNull(document, "korkeustarkkuus", feature);
+        appendValueIfNotNull(document, "aineistolahde", feature);
+        appendValueIfNotNull(document, "alkupvm", feature);
+        appendValueIfNotNull(document, "suunta", feature);
+        appendValueIfNotNull(document, "sijainti", feature);
+        appendValueIfNotNull(document, "kohderyhma", feature);
+        appendValueIfNotNull(document, "kohdeluokka", feature);
+        appendValueIfNotNull(document, "numero", feature);
+        appendValueIfNotNull(document, "nimi_suomi", feature);
+        appendValueIfNotNull(document, "nimi_ruotsi", feature);
+        appendValueIfNotNull(document, "kuntatunnus", feature);
+
+        logger.debug("Created {}", document);
+        return document;
+    }
+}
